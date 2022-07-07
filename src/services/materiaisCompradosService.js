851e66f683e0f420a2fb77cff150660b410e315a
pepz1n <bernardo.zanetti@unochapeco.edu.br>
2022-07-07 17:04:42 -0300

    feat: melhoria nas api de compra e gastos que agora mudam os vaores da tabela materiais

--- src/services/materiaisCompradosService.js
+++ src/services/materiaisCompradosService.js
@@ -15,21 +15,72 @@ const getmateriaisCompradosById = async (params) =>{
     return materiaisComprados.rows;
 }
 
-//inserir novos clientes
+// //inserir novos clientes
 
-const postmateriaisComprados = async(params) =>{
-    let {valor_compra, id_fornecedor, id_materiais, quantidade} = params;
+// const postmateriaisComprados = async(params) =>{
+//     let {valor_compra, id_fornecedor, id_materiais, quantidade} = params;
+//     let sql = `
+//         insert into materiais_comprados(
+//             valor_compra,
+//             id_fornecedor,
+//             id_materiais,
+//             quantidade
+//         ) values ($1, $2, $3, $4) returning id`;
+//     let insert = await db.query(sql, [valor_compra, id_fornecedor, id_materiais, quantidade])
+//     return insert.rows[0];
+// }
+//inserir novo cadastro de material gasto
+
+const postmateriaisComprados = async (params) =>{
+    //função para pegar o estoque atual e reduir pelo gasto
+    const updateEstoque = async (quantidade,id_material) =>{
+        let sql = `select 
+                    quantidade_estoque 
+                   from materiais 
+                   where id = $1
+                   `;
+        let estoqueAtual = await db.query(sql,[id_material])
+        estoqueAtual = estoqueAtual.rows[0].quantidade_estoque;
+        let estoqueDepoisCompra = estoqueAtual + quantidade;
+        if(estoqueDepoisCompra>=0){
+            return estoqueDepoisCompra;
+        }else{
+            return 0;
+        }
+    }
+    //INSERT DOS MATERIAIS GASTOS NO 'MATERIAIS_GASTOS'
+    let {valor_compra,id_fornecedor, id_materiais, quantidade} = params;
+    let estoqueNovo = await updateEstoque(quantidade,id_materiais);
     let sql = `
         insert into materiais_comprados(
             valor_compra,
             id_fornecedor,
             id_materiais,
             quantidade
-        ) values ($1, $2, $3, $4) returning id`;
-    let insert = await db.query(sql, [valor_compra, id_fornecedor, id_materiais, quantidade])
-    return insert.rows[0];
-}
+        ) values ($1, $2, $3, $4)`;
+    await db.query(sql, [valor_compra, id_fornecedor, id_materiais, quantidade]);
 
+    //select do nome do fornecedor com o id 'id_materiais'
+    let sql3 = `select
+                nome
+                from fornecedor
+                where id= $1`
+    let nome_fornecedor = await db.query (sql3, [id_fornecedor])
+    nome_fornecedor = nome_fornecedor.rows[0].nome
+
+    //update da tabela materiais com o valor novo
+    let sql2 = `update materiais set 
+                    quantidade_estoque = $1 
+                where id = $2 returning nome, quantidade_estoque`
+    let insert = await db.query (sql2, [estoqueNovo, id_materiais])
+    //return com a mensagem
+    return  {
+        nome_nome_fornecedor: nome_fornecedor,
+        id_fornecedor,
+        quantidade_comprado: quantidade,
+        materiais: insert.rows[0]
+    };
+}
 
 // deletar um cliente
 const deletemateriaisComprados = async (params) =>{
